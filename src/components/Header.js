9204a10f37d399fd1a1c361296a7f9dd79372082
Moustapha Kachab <moustaphakachab@esp.sn>
2021-11-01 12:41:40 +0000

    Adding typewrite effect library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,43 @@
-import './../styles/Header.css';
-import Navbar from './Navbar';
-import React from 'react'
+import "./../styles/Header.css";
+import Navbar from "./Navbar";
+import React from "react";
+import TypewriterComponent from "typewriter-effect";
 
-function Header () {
-    return (
-        <header className="mossane-header container column column-left">
-            <Navbar />
-            <hgroup>
-                <h1>MOSSANE</h1>
-                <h2>Patrimoine</h2>
-            </hgroup>
-            <p>
-                À vos côtés pour la réussite <br /> de vos projets.
-            </p>
-            <div className="mossane-scrolldown">
-                <span>Balayez vers le bas</span>
-                <div className="mossane-scrolldown-anim"> </div>
-            </div>
-        </header>
-    )
+function Header() {
+  return (
+    <header className="mossane-header container column column-left">
+      <Navbar />
+      <hgroup>
+        <h1>
+          <TypewriterComponent
+            onInit={(typewriter) => {
+              typewriter.typeString("MOSSANE").start();
+            }}
+          />
+        </h1>
+        <h2>
+          <TypewriterComponent
+            onInit={(typewriter) => {
+                
+              typewriter.pauseFor(1500).typeString("Patrimoine").start();
+            }}
+          />
+        </h2>
+      </hgroup>
+      <p>
+        <TypewriterComponent
+            onInit={(typewriter) => {
+                
+              typewriter.pauseFor(3000).typeString("À vos côtés pour la réussite <br /> de vos projets.").start();
+            }}
+          />   
+        
+      </p>
+      <div className="mossane-scrolldown">
+        <span>Balayez vers le bas</span>
+        <div className="mossane-scrolldown-anim"> </div>
+      </div>
+    </header>
+  );
 }
-export default Header
+export default Header;
